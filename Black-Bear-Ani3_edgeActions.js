d4c69594d2e64eaa49b6374e8aae2d67f9380b70
Dan Matthews <dan@bluefoc.us>
2014-07-16 10:14:34 -0400

    initial commit

---
+++ Black-Bear-Ani3_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",9867,function(sym,e){sym.play("loop");sym.target-Object.play(1000);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-324268247");
